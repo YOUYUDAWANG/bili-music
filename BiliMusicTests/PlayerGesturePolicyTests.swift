58e673c64e0c13f9f0bd75fe1f486ed94fc3a8d2
Chaoyi Liu <liuchaoyi@akane.waseda.jp>
2026-06-27 08:11:31 +0900

    test(03-03): add failing gesture conflict policy coverage

--- BiliMusicTests/PlayerGesturePolicyTests.swift
+++ BiliMusicTests/PlayerGesturePolicyTests.swift
@@ -66,6 +66,83 @@ final class PlayerGesturePolicyTests: XCTestCase {
             startY: 80))
     }
 
+    func testCenterBodyDismissTracksAndRequiresDeliberateDrag() {
+        XCTAssertEqual(PlayerGesturePolicy.dismissDragOffset(
+            translation: CGSize(width: 0, height: 92),
+            startY: 420,
+            region: .centerBody,
+            isProgressScrubbing: false), 92)
+        XCTAssertFalse(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 92),
+            predictedEndTranslation: CGSize(width: 0, height: 120),
+            startY: 420,
+            region: .centerBody,
+            isProgressScrubbing: false))
+
+        XCTAssertTrue(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 146),
+            predictedEndTranslation: CGSize(width: 0, height: 170),
+            startY: 420,
+            region: .centerBody,
+            isProgressScrubbing: false))
+        XCTAssertTrue(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 70),
+            predictedEndTranslation: CGSize(width: 0, height: 280),
+            startY: 420,
+            region: .centerBody,
+            isProgressScrubbing: false))
+    }
+
+    func testListBodyDismissNeverTracksEvenWhenLarge() {
+        XCTAssertNil(PlayerGesturePolicy.dismissDragOffset(
+            translation: CGSize(width: 0, height: 260),
+            startY: 220,
+            region: .listBody,
+            isProgressScrubbing: false))
+        XCTAssertFalse(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 260),
+            predictedEndTranslation: CGSize(width: 0, height: 520),
+            startY: 220,
+            region: .listBody,
+            isProgressScrubbing: false))
+    }
+
+    func testTopChromeRegionDismissWorksAcrossPages() {
+        XCTAssertEqual(PlayerGesturePolicy.dismissDragOffset(
+            translation: CGSize(width: 0, height: 72),
+            startY: 40,
+            region: .topChrome,
+            isProgressScrubbing: false), 72)
+        XCTAssertTrue(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 95),
+            predictedEndTranslation: CGSize(width: 0, height: 120),
+            startY: 40,
+            region: .topChrome,
+            isProgressScrubbing: false))
+    }
+
+    func testHorizontalIntentAndScrubSuppressVerticalDismiss() {
+        XCTAssertNil(PlayerGesturePolicy.dismissDragOffset(
+            translation: CGSize(width: 0, height: 180),
+            startY: 60,
+            region: .topChrome,
+            isProgressScrubbing: true))
+        XCTAssertFalse(PlayerGesturePolicy.shouldDismissFullPlayer(
+            translation: CGSize(width: 0, height: 180),
+            predictedEndTranslation: CGSize(width: 0, height: 280),
+            startY: 60,
+            region: .topChrome,
+            isProgressScrubbing: true))
+        XCTAssertFalse(PlayerGesturePolicy.isHorizontalPageSwipe(
+            translation: CGSize(width: 52, height: 88),
+            predictedEndTranslation: CGSize(width: 64, height: 124),
+            width: 390))
+        XCTAssertTrue(PlayerGesturePolicy.isHorizontalPageSwipe(
+            translation: CGSize(width: -86, height: 12),
+            predictedEndTranslation: CGSize(width: -116, height: 18),
+            width: 390))
+    }
+
     func testFullPlayerDismissRequiresDeliberateTopDownDragOrPrediction() {
         XCTAssertEqual(PlayerGesturePolicy.dismissDragOffset(
             translation: CGSize(width: 0, height: 96),
